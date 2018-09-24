976694c24285a42f19fed294791ef86c17fee09c
Daniel Quah <danielquah148@yahoo.com>
2018-09-24 22:40:12 +0800

    Added transcation filters
    - Allow users to filter by week(not working yet), month and year
    - Added overflow menu to allow users to choose their filter

--- app/src/main/java/xyz/hisname/fireflyiii/ui/transaction/TransactionFragment.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/transaction/TransactionFragment.kt
@@ -2,9 +2,9 @@ package xyz.hisname.fireflyiii.ui.transaction
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
@@ -17,6 +17,8 @@ import xyz.hisname.fireflyiii.repository.models.transaction.TransactionApiRespon
 import xyz.hisname.fireflyiii.repository.models.transaction.TransactionData
 import xyz.hisname.fireflyiii.repository.viewmodel.retrofit.TransactionViewModel
 import xyz.hisname.fireflyiii.ui.base.BaseFragment
+import xyz.hisname.fireflyiii.util.DateTimeUtil
+import xyz.hisname.fireflyiii.util.extension.consume
 import xyz.hisname.fireflyiii.util.extension.create
 import xyz.hisname.fireflyiii.util.extension.getViewModel
 import java.util.*
@@ -32,20 +34,27 @@ class TransactionFragment: BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        if(!Objects.equals(transactionType, "all")) {
+            setHasOptionsMenu(true)
+        }
         return inflater.create(R.layout.fragment_dashboard_recent_transaction, container)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if(!Objects.equals(transactionType, "all")){
+            val toolbar = requireActivity().findViewById<Toolbar>(R.id.activity_toolbar)
+            toolbar.overflowIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_filter)
             recentTransactionText.isVisible = false
             linedivider.isVisible = false
         }
-        loadTransaction(transactionType, startDate, endDate)
+        loadTransaction(startDate, endDate)
     }
 
-    private fun loadTransaction(transactionType: String, startDate: String?, endDate: String?){
+    private fun loadTransaction(startDate: String?, endDate: String?){
+        dataAdapter.clear()
         transactionLoader.bringToFront()
+        transactionLoader.show()
         recentTransactionList.layoutManager = LinearLayoutManager(requireContext())
         recentTransactionList.addItemDecoration(DividerItemDecoration(recentTransactionList.context,
                 DividerItemDecoration.VERTICAL))
@@ -66,6 +75,8 @@ class TransactionFragment: BaseFragment() {
     private fun processData(transactionType: String, response: TransactionApiResponse){
         if(Objects.equals(transactionType, "all")) {
             if (dataAdapter.size <= 5) {
+                recentTransactionList.isVisible = true
+                noTransactionText.isGone = true
                 rtAdapter = TransactionRecyclerAdapter(response.getTransaction()?.data!!.toMutableList())
                 recentTransactionList.adapter = rtAdapter
                 rtAdapter.apply {
@@ -73,6 +84,8 @@ class TransactionFragment: BaseFragment() {
                     update(response.getTransaction()?.data!!.toMutableList())
                 }
             } else {
+                recentTransactionList.isVisible = true
+                noTransactionText.isGone = true
                 // More than 5 index in json so we get first 5 only
                 dataAdapter.subList(5, dataAdapter.size).clear()
                 rtAdapter = TransactionRecyclerAdapter(dataAdapter)
@@ -83,6 +96,8 @@ class TransactionFragment: BaseFragment() {
                 }
             }
         } else {
+            recentTransactionList.isVisible = true
+            noTransactionText.isGone = true
             rtAdapter = TransactionRecyclerAdapter(response.getTransaction()?.data!!.toMutableList())
             recentTransactionList.adapter = rtAdapter
             rtAdapter.apply {
@@ -90,6 +105,8 @@ class TransactionFragment: BaseFragment() {
                 update(response.getTransaction()?.data!!.toMutableList())
             }
         }
+        rtAdapter.notifyDataSetChanged()
+        println("response: " + response.getTransaction()?.data)
     }
 
     override fun onAttach(context: Context){
@@ -106,6 +123,24 @@ class TransactionFragment: BaseFragment() {
             activity?.activity_toolbar?.title = transactionType.substring(0,1).toUpperCase() +
                     transactionType.substring(1)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.filter_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when(item?.itemId) {
+        R.id.menu_item_week -> consume {
+           // loadTransaction()
+        }
+        R.id.menu_item_month -> consume {
+            loadTransaction(DateTimeUtil.getStartOfMonth(), DateTimeUtil.getEndOfMonth())
+        }
+        R.id.menu_item_year -> consume {
+            loadTransaction(DateTimeUtil.getStartOfYear(), DateTimeUtil.getEndOfYear())
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
 }
