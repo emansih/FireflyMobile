10dcee468dc2b1acfbe3220be0e129002dad1b62
Daniel Quah <danielquah148@yahoo.com>
2018-11-24 20:29:09 +0800

    Fix compile error

--- app/src/main/java/xyz/hisname/fireflyiii/data/local/dao/BudgetLimitDataDao.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/data/local/dao/BudgetLimitDataDao.kt
@@ -3,7 +3,6 @@ package xyz.hisname.fireflyiii.data.local.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
-import xyz.hisname.fireflyiii.repository.models.budget.budget.BudgetData
 import xyz.hisname.fireflyiii.repository.models.budget.limit.BudgetLimitData
 
 @Dao
@@ -14,7 +13,4 @@ abstract class BudgetLimitDataDao: BaseDao<BudgetLimitData> {
 
     @Query("SELECT * FROM budget_limit WHERE (start_date =:startDate AND end_date =:endDate)")
     abstract fun getConstraintBudget(startDate: String, endDate: String): MutableList<BudgetLimitData>
-
-    @Query("SELECT * FROM budget_limit")
-    abstract fun currencyIdBudget(): MutableList<BudgetData>
 }
