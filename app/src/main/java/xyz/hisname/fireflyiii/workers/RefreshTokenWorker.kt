fe54ba75827f8454f051b9626991a3cdd1caa5c0
Daniel Quah <danielquah148@yahoo.com>
2018-11-28 19:53:47 +0800

    update refresh token worker

--- app/src/main/java/xyz/hisname/fireflyiii/workers/RefreshTokenWorker.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/workers/RefreshTokenWorker.kt
@@ -2,30 +2,32 @@ package xyz.hisname.fireflyiii.workers
 
 import android.content.Context
 import androidx.work.*
-import retrofit2.Call
 import xyz.hisname.fireflyiii.data.local.pref.AppPref
-import xyz.hisname.fireflyiii.data.remote.RetrofitBuilder
 import xyz.hisname.fireflyiii.data.remote.api.OAuthService
-import xyz.hisname.fireflyiii.repository.models.auth.AuthModel
 
 class RefreshTokenWorker(private val context: Context, workerParameters: WorkerParameters): BaseWorker(context, workerParameters)  {
 
     override fun doWork(): Result {
-        return if(service()?.execute()?.isSuccessful!!){
-            Result.SUCCESS
+        val execution = service()?.execute()
+        return if(execution != null){
+            val responseBody = execution.body()
+            if(responseBody != null){
+                AppPref(context).apply {
+                    accessToken = responseBody.access_token
+                    refreshToken = responseBody.refresh_token
+                    tokenExpiry = responseBody.expires_in
+                }
+                Result.SUCCESS
+            } else {
+                Result.FAILURE
+            }
         } else {
             Result.RETRY
         }
     }
 
-    private fun service(): Call<AuthModel>?{
-        var cert = ""
-        if(AppPref(context).enableCertPinning){
-            cert = AppPref(context).certValue
-        }
-        return RetrofitBuilder.getClient(AppPref(context).baseUrl,
-                AppPref(context).accessToken, cert)?.create(OAuthService::class.java)?.getRefreshToken("refresh_token",
+    private fun service() = genericService?.create(OAuthService::class.java)?.getRefreshToken("refresh_token",
                 AppPref(context).refreshToken, AppPref(context).clientId,
                 AppPref(context).secretKey)
-    }
+
 }
