97279c7bf92cb68b104d683ccc4769a8943e607b
Daniel Quah <danielquah148@yahoo.com>
2018-12-05 21:55:30 +0800

    Redesign add bill screen

--- app/src/main/java/xyz/hisname/fireflyiii/ui/bills/AddBillActivity.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/bills/AddBillActivity.kt
@@ -2,16 +2,19 @@ package xyz.hisname.fireflyiii.ui.bills
 
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color.rgb
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
-import com.google.gson.Gson
+import com.mikepenz.fontawesome_typeface_library.FontAwesome
+import com.mikepenz.iconics.IconicsDrawable
 import kotlinx.android.synthetic.main.activity_add_bill.*
+import kotlinx.android.synthetic.main.fragment_add_transaction.*
 import kotlinx.android.synthetic.main.progress_overlay.*
 import xyz.hisname.fireflyiii.Constants
 import xyz.hisname.fireflyiii.R
@@ -35,33 +38,63 @@ class AddBillActivity: BaseActivity() {
     private var repeatFreq: String = ""
     private lateinit var currency: String
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_bill)
-        if(intent.getStringExtra("status") != null) {
+        setSupportActionBar(placeHolderToolbar)
+        setWidgets()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+        setIcons()
+        placeHolderToolbar.setNavigationOnClickListener {
+            checkEmptiness()
+        }
+        addBill()
+        /*
+         if(intent.getStringExtra("status") != null) {
             billAttribute = Gson().fromJson(intent.getSerializableExtra("billData").toString(),
                     BillAttributes::class.java)
         }
-        setupWidgets()
-        currencyViewModel.currencyCode.observe(this, Observer {
-            currency = it
-        })
-
-        currencyViewModel.currencyDetails.observe(this, Observer {
-            currency_code_edittext.setText(it)
-        })
+         */
     }
 
-    private fun setupWidgets(){
-        setSupportActionBar(bill_create_toolbar)
-        bill_create_toolbar.setNavigationOnClickListener {
-            checkEmptiness()
-        }
+    private fun setIcons(){
+        currency_edittext.setCompoundDrawablesWithIntrinsicBounds(
+                IconicsDrawable(this).icon(FontAwesome.Icon.faw_money_bill)
+                        .color(ContextCompat.getColor(this, R.color.md_green_400))
+                        .sizeDp(24),null, null, null)
+        min_amount_edittext.setCompoundDrawablesWithIntrinsicBounds(
+                IconicsDrawable(this).icon(FontAwesome.Icon.faw_dollar_sign)
+                        .color(ContextCompat.getColor(this, R.color.md_yellow_A700))
+                        .sizeDp(16),null, null, null)
+        max_amount_edittext.setCompoundDrawablesWithIntrinsicBounds(
+                IconicsDrawable(this).icon(FontAwesome.Icon.faw_dollar_sign)
+                        .color(ContextCompat.getColor(this, R.color.md_yellow_A700))
+                        .sizeDp(16),null, null, null)
+        bill_date_edittext.setCompoundDrawablesWithIntrinsicBounds(IconicsDrawable(this)
+                .icon(FontAwesome.Icon.faw_calendar)
+                .color(ColorStateList.valueOf(rgb(18, 122, 190)))
+                .sizeDp(24),null, null, null)
+        rules_edittext.setCompoundDrawablesWithIntrinsicBounds(
+                IconicsDrawable(this).icon(FontAwesome.Icon.faw_ruler)
+                        .color(ContextCompat.getColor(this, R.color.md_brown_500))
+                        .sizeDp(24),null, null, null)
+        skip_edittext.setCompoundDrawablesWithIntrinsicBounds(
+                IconicsDrawable(this).icon(FontAwesome.Icon.faw_sort_numeric_up)
+                        .color(ContextCompat.getColor(this, R.color.md_black_1000))
+                        .sizeDp(24),null, null, null)
+        addBillFab.setBackgroundColor(ContextCompat.getColor(this, R.color.primary_dark))
+        addBillFab.setImageDrawable(IconicsDrawable(this).icon(FontAwesome.Icon.faw_save)
+                .color(ContextCompat.getColor(this, R.color.md_black_1000))
+                .sizeDp(24))
+    }
+
+    private fun setWidgets(){
         val spinnerValue = arrayOf(Constants.WEEKLY, Constants.MONTHLY,
                 Constants.QUARTERLY, Constants.HALF_YEARLY, Constants.YEARLY)
         val dataAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, spinnerValue)
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        repeat_spinner.adapter = dataAdapter
+        frequency_spinner.adapter = dataAdapter
         val calendar = Calendar.getInstance()
         val startDate = DatePickerDialog.OnDateSetListener {
             _, year, monthOfYear, dayOfMonth ->
@@ -72,86 +105,77 @@ class AddBillActivity: BaseActivity() {
                 bill_date_edittext.setText(DateTimeUtil.getCalToString(calendar.timeInMillis.toString()))
             }
         }
-
         bill_date_edittext.setOnClickListener {
+            hideKeyboard()
             DatePickerDialog(this, startDate, calendar.get(Calendar.YEAR),
                     calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
                     .show()
         }
-        if(intent.getStringExtra("status") != null){
-            bill_name_edittext.setText(billAttribute?.name)
-            amount_min_edittext.setText(billAttribute?.amount_min.toString())
-            amount_max_edittext.setText(billAttribute?.amount_max.toString())
-            bill_date_edittext.setText(billAttribute?.date.toString())
-            val data = billAttribute?.repeat_freq?.substring(0,1)?.toUpperCase() + billAttribute?.repeat_freq?.substring(1)
-            val spinnerPosition = dataAdapter.getPosition(data)
-            repeat_spinner.setSelection(spinnerPosition, true)
-            skip_edittext.setText(billAttribute?.skip.toString())
-            currency_code_edittext.setText(billAttribute?.currency_code)
-            note_edittext.setText(billAttribute?.markdown)
-        }
-        currency_code_edittext.setOnClickListener{
+        currency_edittext.setOnClickListener{
+            hideKeyboard()
             val currencyListFragment = CurrencyListFragment()
             currencyListFragment.show(supportFragmentManager, "currencyList" )
-
         }
+        currencyViewModel.currencyCode.observe(this, Observer {
+            currency = it
+        })
+
+        currencyViewModel.currencyDetails.observe(this, Observer {
+            currency_edittext.setText(it)
+        })
     }
 
     private fun validateInput(): Boolean{
-        var shouldContinue = true
-        if(amount_max_edittext.isBlank()){
-            amount_max_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+        if(min_amount_edittext.isBlank()){
+            min_amount_layout.error = resources.getString(R.string.required_field)
+            return false
         }
-        if(amount_min_edittext.isBlank()){
-            amount_min_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+        if(max_amount_edittext.isBlank()){
+            max_amount_layout.error = resources.getString(R.string.required_field)
+            return false
         }
-        if(shouldContinue) {
-            if (amount_max_edittext.getDigits() < amount_min_edittext.getDigits()) {
-                amount_max_edittext.error = "Max amount should be more than min amount"
-                amount_min_edittext.error = "Min amount should be less than max amount"
-                shouldContinue = false
-            }
+        if (max_amount_edittext.getDigits() < min_amount_edittext.getDigits()) {
+            max_amount_layout.error = "Max amount should be more than min amount"
+            min_amount_layout.error = "Min amount should be less than max amount"
+            return false
         }
-        if(bill_name_edittext.isBlank()){
-            bill_name_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+        if(description_edittext.isBlank()){
+            description_edittext.error = resources.getString(R.string.required_field)
+            return false
         }
-        if(amount_min_edittext.isBlank()) {
-            amount_min_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+        if(min_amount_edittext.isBlank()) {
+            min_amount_edittext.error = resources.getString(R.string.required_field)
+            return false
         }
-        if(amount_max_edittext.isBlank()){
-            amount_max_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+        if(max_amount_edittext.isBlank()){
+            max_amount_layout.error = resources.getString(R.string.required_field)
+            return false
         }
 
         if(bill_date_edittext.isBlank()){
-            bill_date_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+            bill_date_layout.error = resources.getString(R.string.required_field)
+            return false
         }
         if(!skip_edittext.isDigitsOnly()){
-            skip_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+            skip_layout.error = resources.getString(R.string.required_field)
+            return false
         }
-
         if(skip_edittext.isBlank()){
-            skip_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+            skip_layout.error = resources.getString(R.string.required_field)
+            return false
         }
-        if(currency_code_edittext.isBlank()){
-            currency_code_edittext.error = resources.getString(R.string.required_field)
-            shouldContinue = false
+        if(currency_edittext.isBlank()){
+            currency_layout.error = resources.getString(R.string.required_field)
+            return false
         }
-        return shouldContinue
+        return true
     }
 
     private fun checkEmptiness(){
         if(skip_edittext.isBlank() && bill_date_edittext.isBlank() &&
-                note_edittext.isBlank() && amount_max_edittext.isBlank() &&
-                amount_min_edittext.isBlank() && bill_name_edittext.isBlank()
-        && bill_match_edittext.isBlank() && currency_code_edittext.isBlank()){
+                notes_edittext.isBlank() && max_amount_edittext.isBlank() &&
+                min_amount_edittext.isBlank() && description_edittext.isBlank()
+                && rules_edittext.isBlank() && currency_edittext.isBlank()){
             toastInfo("No information entered. Bill not saved")
             finish()
         } else {
@@ -168,87 +192,56 @@ class AddBillActivity: BaseActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.save_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
     private fun addBill(){
-        model.addBill(bill_name_edittext.getString(),
-                bill_match_edittext.getString(),amount_min_edittext.getString(),
-                amount_max_edittext.getString(), bill_date_edittext.getString(), repeatFreq,
-                skip_edittext.getString(), "1", "1",
-                currency, notes)
-                .observe(this, Observer {
-                    ProgressBar.animateView(progress_overlay, View.GONE, 0.toFloat(), 200)
-                    val errorMessage = it.getErrorMessage()
-                    if(errorMessage != null) {
-                       toastError(errorMessage)
-                    } else if(it.getError() != null){
-                        if (it.getError()!!.localizedMessage.startsWith("Unable to resolve host")) {
-                            val billBroadcast = Intent(this, BillReceiver::class.java).apply {
-                                action = "firefly.hisname.ADD_BILL"
-                            }
-                            val extras = bundleOf(
-                                    "name" to bill_name_edittext.getString(),
-                                    "billMatch" to bill_match_edittext.getString(),
-                                    "minAmount" to amount_min_edittext.getString(),
-                                    "maxAmount" to amount_max_edittext.getString(),
-                                    "billDate" to bill_date_edittext.getString(),
-                                    "repeatFreq" to repeatFreq,
-                                    "skip" to skip_edittext.getString(),
-                                    "currencyCode" to currency,
-                                    "notes" to notes)
-                            billBroadcast.putExtras(extras)
-                            sendBroadcast(billBroadcast)
-                            toastOffline(getString(R.string.data_added_when_user_online, "Bill"))
-                            finish()
-                        } else {
-                            toastError("Error saving bill")
-                        }
-                    } else if(it.getResponse() != null){
-                        toastSuccess("Bill saved")
-                        finish()
-                    }
-
-                })
-    }
-
-    private fun updateBill(){
-        model.updateBill(intent.getLongExtra("billId", 0).toString(),
-                bill_name_edittext.getString(), bill_match_edittext.getString(),amount_min_edittext.getString(),
-                amount_max_edittext.getString(), bill_date_edittext.getString(), repeatFreq,
-                skip_edittext.getString(), "1", "1",
-                currency, notes).observe(this, Observer {
-            ProgressBar.animateView(progress_overlay, View.GONE, 0.toFloat(), 200)
-            if(it.getErrorMessage() != null) {
-
-            } else {
-
-            }
-        })
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == R.id.menu_item_save){
+        addBillFab.setOnClickListener {
             hideKeyboard()
-            if(validateInput()){
+            notes = if(notes_edittext.isBlank()){
+                null
+            } else {
+                notes_edittext.getString()
+            }
+            repeatFreq = frequency_spinner.selectedItem.toString().substring(0,1).toLowerCase() +
+                    frequency_spinner.selectedItem.toString().substring(1)
+            if(validateInput()) {
                 ProgressBar.animateView(progress_overlay, View.VISIBLE, 0.4f, 200)
-                notes = if(note_edittext.isBlank()){
-                    null
-                } else {
-                    note_edittext.getString()
-                }
-                repeatFreq = repeat_spinner.selectedItem.toString().substring(0,1).toLowerCase() +
-                        repeat_spinner.selectedItem.toString().substring(1)
-                if(intent.getStringExtra("status") != null){
-                    updateBill()
-                } else {
-                    addBill()
-                }
+                model.addBill(description_edittext.getString(),
+                        rules_edittext.getString(), min_amount_edittext.getString(),
+                        max_amount_edittext.getString(), bill_date_edittext.getString(), repeatFreq,
+                        skip_edittext.getString(), "1", "1",
+                        currency, notes)
+                        .observe(this, Observer { response ->
+                            ProgressBar.animateView(progress_overlay, View.GONE, 0.toFloat(), 200)
+                            val errorMessage = response.getErrorMessage()
+                            if (errorMessage != null) {
+                                toastError(errorMessage)
+                            } else if (response.getError() != null) {
+                                val billBroadcast = Intent(this, BillReceiver::class.java).apply {
+                                    action = "firefly.hisname.ADD_BILL"
+                                }
+                                val extras = bundleOf(
+                                        "name" to description_edittext.getString(),
+                                        "billMatch" to rules_edittext.getString(),
+                                        "minAmount" to min_amount_edittext.getString(),
+                                        "maxAmount" to max_amount_edittext.getString(),
+                                        "billDate" to bill_date_edittext.getString(),
+                                        "repeatFreq" to repeatFreq,
+                                        "skip" to skip_edittext.getString(),
+                                        "currencyCode" to currency,
+                                        "notes" to notes)
+                                billBroadcast.putExtras(extras)
+                                sendBroadcast(billBroadcast)
+                                toastOffline(getString(R.string.data_added_when_user_online, "Bill"))
+                                finish()
+                            } else if (response.getResponse() != null) {
+                                toastSuccess("Bill saved")
+                                finish()
+                            }
+
+                        })
+            } else {
+                toastInfo("Please check for errors")
             }
         }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
