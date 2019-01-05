d6cc732ad7811cb84875862a1425e1f7c670b27c
Daniel Quah <danielquah148@yahoo.com>
2019-01-05 16:34:56 +0800

    Reduce report content data

--- app/src/main/java/xyz/hisname/fireflyiii/CustomApp.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/CustomApp.kt
@@ -3,6 +3,7 @@ package xyz.hisname.fireflyiii
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.acra.ACRA
+import org.acra.ReportField
 import org.acra.annotation.AcraCore
 import org.acra.annotation.AcraMailSender
 import org.acra.data.StringFormat
@@ -10,7 +11,10 @@ import org.acra.sender.EmailIntentSenderFactory
 import xyz.hisname.languagepack.LanguageChanger
 
 @AcraCore(reportFormat = StringFormat.KEY_VALUE_LIST,
-        reportSenderFactoryClasses = [EmailIntentSenderFactory::class], buildConfigClass = BuildConfig::class)
+        reportSenderFactoryClasses = [EmailIntentSenderFactory::class], buildConfigClass = BuildConfig::class,
+        reportContent = [ReportField.REPORT_ID, ReportField.APP_VERSION_NAME,
+        ReportField.PHONE_MODEL, ReportField.BRAND, ReportField.PRODUCT, ReportField.ANDROID_VERSION,
+        ReportField.BUILD_CONFIG, ReportField.STACK_TRACE, ReportField.LOGCAT])
 @AcraMailSender(reportAsFile = true, mailTo = "", resSubject = R.string.app_name,
         reportFileName = "Fireflyiii-mobile.txt")
 class CustomApp: Application() {
