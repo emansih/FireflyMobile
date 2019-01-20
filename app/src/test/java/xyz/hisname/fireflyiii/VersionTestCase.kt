19260afad81abb1e24c9c3a1236c990e25bda0d9
Daniel Quah <danielquah148@yahoo.com>
2019-01-20 11:33:00 +0800

    Added version test case

---
+++ app/src/test/java/xyz/hisname/fireflyiii/VersionTestCase.kt
@@ -0,0 +1,38 @@
+package xyz.hisname.fireflyiii
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import xyz.hisname.fireflyiii.util.Version
+
+class VersionTestCase {
+
+    @Test
+    fun isVersionLower(){
+        val versionCode = Version("1.1.1").compareTo(Version("1.1.2"))
+        assertEquals(-1, versionCode)
+    }
+
+    @Test
+    fun isVersionEquals(){
+        val isEquals = Version("1.1.1") == Version("1.1.1")
+        assertEquals(true, isEquals)
+    }
+
+    @Test
+    fun isVersionHigher(){
+        val versionCode = Version("1.1.2").compareTo(Version("1.1.1"))
+        assertEquals(1, versionCode)
+    }
+
+    @Test
+    fun isVersionNotEquals(){
+        val isEquals = Version("1.1.1") == Version("1.1.0")
+        assertEquals(false, isEquals)
+    }
+
+    @Test
+    fun isVersionWithDecimalEquals(){
+        val isEquals = Version("1.0") == Version("1")
+        assertEquals(true, isEquals)
+    }
+}
