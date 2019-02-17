23f687867fca3e180879f9fb07fdea10f0aecba9
Daniel Quah <danielquah148@yahoo.com>
2019-02-17 11:38:00 +0800

    Bump libraries
    - Update kotlin stdlib to 1.3.21
    - Update coroutines lib to 1.1.1
    - Update work manager to 1.0.0-rc01
    - Update pref lib to 1.1.0-alpha03
    - Update toasty to 1.4.0
    - Update nachos to 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven("https://dl.bintray.com/florent37/maven")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0")
+        classpath("com.android.tools.build:gradle:3.3.1")
         classpath(kotlin("gradle-plugin", Dependencies.kotlinVersion))
         classpath("com.akaita.android:easylauncher:1.3.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
