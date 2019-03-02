ec0935785d879e52f086703e049d1bb64596d649
Daniel Quah <danielquah148@yahoo.com>
2019-03-02 21:07:05 +0800

    Removed unused Gson converter function

--- app/src/main/java/xyz/hisname/fireflyiii/util/GsonConverterUtil.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/util/GsonConverterUtil.kt
@@ -5,15 +5,8 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDateTime
-import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.ZoneOffset
-import org.threeten.bp.format.DateTimeFormatter
-import xyz.hisname.fireflyiii.repository.models.bills.BillAttributes
-import xyz.hisname.fireflyiii.repository.models.bills.Relationships
 import xyz.hisname.fireflyiii.repository.models.budget.budgetList.Spent
-import xyz.hisname.fireflyiii.repository.models.category.CategoryAttributes
-import xyz.hisname.fireflyiii.repository.models.currency.CurrencyAttributes
-import xyz.hisname.fireflyiii.repository.models.piggy.PiggyAttributes
 import java.math.BigDecimal
 
 object GsonConverterUtil{
@@ -69,66 +62,6 @@ object GsonConverterUtil{
     fun toBudgetSpent(spent: String): List<Spent>{
         val type = object : TypeToken<List<Spent>>() {}.type
         return Gson().fromJson(spent,type)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun toCategoryAttribute(categoryAttributes: CategoryAttributes): String {
-        return Gson().toJson(categoryAttributes)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun fromCategoryAttributes(attributes: String): CategoryAttributes {
-        return Gson().fromJson(attributes, CategoryAttributes::class.java)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun toCurrencyAttributes(currencyAttributes: CurrencyAttributes): String {
-        return Gson().toJson(currencyAttributes)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun fromCurrencyAttributes(attributes: String): CurrencyAttributes {
-        return Gson().fromJson(attributes, CurrencyAttributes::class.java)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun toPiggyAttributes(piggyAttributes: PiggyAttributes): String {
-        return Gson().toJson(piggyAttributes)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun fromPiggyAttributes(attributes: String): PiggyAttributes {
-        return Gson().fromJson(attributes, PiggyAttributes::class.java)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun toBillAttributes(billAttributes: BillAttributes): String {
-        return Gson().toJson(billAttributes)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun fromBillAttributes(attributes: String): BillAttributes {
-        return Gson().fromJson(attributes, BillAttributes::class.java)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun toRelationShips(relationship: Relationships): String{
-        return Gson().toJson(relationship)
-    }
-
-    @TypeConverter
-    @JvmStatic
-    fun fromRelationShips(relationships: String): Relationships {
-        return Gson().fromJson(relationships, Relationships::class.java)
     }
 
     @TypeConverter
