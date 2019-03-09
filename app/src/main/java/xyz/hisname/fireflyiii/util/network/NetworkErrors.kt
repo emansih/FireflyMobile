0f1194cd04581ff6d1cfb509576dad830bacb8c9
Daniel Quah <danielquah148@yahoo.com>
2019-03-09 20:13:13 +0800

    Inform user to use https

--- app/src/main/java/xyz/hisname/fireflyiii/util/network/NetworkErrors.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/util/network/NetworkErrors.kt
@@ -6,6 +6,8 @@ class NetworkErrors(){
         fun getThrowableMessage(message: String): String{
             return if(message.startsWith("Certificate Pinning failure")){
                 "Certificate pinning failure!"
+            } else if(message.startsWith("CLEARTEXT communication to")){
+                "Please use HTTPS in your URL"
             } else {
                 message
             }
