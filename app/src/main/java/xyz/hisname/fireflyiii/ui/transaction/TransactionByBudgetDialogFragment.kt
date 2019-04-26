aff8e430a8c8dfc0af9963d708dda79a2de95bfa
Daniel Quah <danielquah148@yahoo.com>
2019-04-26 22:02:34 +0800

    change wording from "Recent transaction" -> "Recent withdrawal"

--- app/src/main/java/xyz/hisname/fireflyiii/ui/transaction/TransactionByBudgetDialogFragment.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/transaction/TransactionByBudgetDialogFragment.kt
@@ -32,6 +32,7 @@ class TransactionByBudgetDialogFragment: DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recentTransactionText.text = "Recent Withdrawal"
         recentTransactionList.layoutManager = LinearLayoutManager(requireContext())
         recentTransactionList.addItemDecoration(DividerItemDecoration(requireContext(),
                 DividerItemDecoration.VERTICAL))
