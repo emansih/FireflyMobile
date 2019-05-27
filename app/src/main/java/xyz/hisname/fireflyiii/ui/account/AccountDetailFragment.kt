834b066049b173361f2fa6aab84be0f3700b8d69
Daniel Quah <danielquah148@yahoo.com>
2019-05-27 19:50:09 +0800

    Fix bug where account details only show default currency when the account is a foreign currency account

--- app/src/main/java/xyz/hisname/fireflyiii/ui/account/AccountDetailFragment.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/account/AccountDetailFragment.kt
@@ -29,9 +29,9 @@ import xyz.hisname.fireflyiii.ui.base.BaseDetailFragment
 import xyz.hisname.fireflyiii.ui.transaction.TransactionRecyclerAdapter
 import xyz.hisname.fireflyiii.ui.transaction.details.TransactionDetailsFragment
 import xyz.hisname.fireflyiii.util.DateTimeUtil
+import xyz.hisname.fireflyiii.util.LocaleNumberParser
 import xyz.hisname.fireflyiii.util.MpAndroidPercentFormatter
 import xyz.hisname.fireflyiii.util.extension.*
-import java.text.DecimalFormat
 import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
@@ -59,139 +59,134 @@ class AccountDetailFragment: BaseDetailFragment() {
         for (col in ColorTemplate.MATERIAL_COLORS) {
             coloring.add(col)
         }
-        currencyViewModel.getDefaultCurrency().observe(this) { currencyData ->
-            if(currencyData.isNotEmpty()){
-                setLineChart(currencyData[0].currencyAttributes?.code ?: "", currencyData[0].currencyAttributes?.symbol ?: "")
+        accountViewModel.getAccountById(accountId).observe(this){ accountData ->
+            val currencyCode = accountData[0].accountAttributes?.currency_code ?: ""
+            val currencySymbol = accountData[0].accountAttributes?.currency_symbol ?: ""
+            val accountName = accountData[0].accountAttributes?.name ?: ""
+            val accountBalance = accountData[0].accountAttributes?.current_balance ?: 0.0
+            currencyViewModel.getCurrencyByCode(currencyCode).observe(this){
+                setLineChart(currencyCode, currencySymbol,  accountName, accountBalance)
+                getAccountTransaction(accountName)
+                setExpensesByCategory(currencyCode, accountName, currencySymbol)
             }
         }
+
         setDarkMode()
         incomePieChart.setNoDataText(resources.getString(R.string.no_data_to_generate_chart))
         budgetPieChart.setNoDataText(resources.getString(R.string.no_data_to_generate_chart))
         categoryPieChart.setNoDataText(resources.getString(R.string.no_data_to_generate_chart))
     }
 
-    private fun setLineChart(currencyCode: String, currencySymbol: String){
-        accountViewModel.getAccountById(accountId).observe(this) { accountData ->
-            accountType = accountData[0].accountAttributes?.type ?: ""
-            if(accountData.isNotEmpty()){
-                zipLiveData(transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 1),
-                        DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 1),
-                        currencyCode, accountData[0].accountAttributes!!.name),
-                        transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 2),
-                                DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 2),
-                                currencyCode, accountData[0].accountAttributes!!.name),
-                        transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 3),
-                                DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 3),
-                                currencyCode, accountData[0].accountAttributes!!.name),
-                        transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 4),
-                                DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 4),
-                                currencyCode, accountData[0].accountAttributes!!.name),
-                        transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 5),
-                                DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 5),
-                                currencyCode, accountData[0].accountAttributes!!.name),
-                        transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
-                                DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
-                                currencyCode, accountData[0].accountAttributes!!.name)).observe(this){ transactionData ->
-                    val firstEntry = accountData[0].accountAttributes?.current_balance?.toBigDecimal()?.plus(transactionData.first)
-                    val secondEntry = firstEntry?.plus(transactionData.second)
-                    val thirdEntry = secondEntry?.plus(transactionData.third)
-                    val fourthEntry = thirdEntry?.plus(transactionData.fourth)
-                    val fifthEntry = fourthEntry?.plus(transactionData.fifth)
-                    val sixthEntry = fifthEntry?.plus(transactionData.sixth)
-                    val lineChartEntries = arrayListOf(
-                            Entry(0f,firstEntry?.toFloat() ?: 0f),
-                            Entry(1f,secondEntry?.toFloat() ?: 0f),
-                            Entry(2f,thirdEntry?.toFloat() ?: 0f),
-                            Entry(3f,fourthEntry?.toFloat() ?: 0f),
-                            Entry(4f,fifthEntry?.toFloat() ?: 0f),
-                            Entry(5f,sixthEntry?.toFloat() ?: 0f)
-                    )
-                    val dataSet = LineDataSet(lineChartEntries, accountData[0].accountAttributes?.name)
-                    dataSet.apply {
-                        setCircleColor(ContextCompat.getColor(requireContext(), R.color.primary))
-                        valueTextColor = Color.BLUE
-                        valueTextSize = 15f
-                    }
-                    val lineChartData = LineData(dataSet)
-
-                    transactionLineChart.apply {
-                        xAxis.granularity = 1f
-                        xAxis.valueFormatter = IndexAxisValueFormatter(arrayListOf(
-                                DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(),1)),
-                                DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 2)),
-                                DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 3)),
-                                DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 4)),
-                                DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 5)),
-                                DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6))))
-                        xAxis.setDrawLabels(true)
-                        xAxis.setDrawAxisLine(false)
-                        xAxis.setDrawGridLines(false)
-                        data = lineChartData
-                        axisRight.isEnabled = false
-                        description.isEnabled = false
-                        xAxis.position = XAxis.XAxisPosition.BOTTOM
-                        data.isHighlightEnabled = false
-                        setTouchEnabled(true)
-                        setPinchZoom(true)
-                        animateY(1000, Easing.EaseOutBack)
-                    }
-                }
-                balanceHistoryCardText.text = resources.getString(R.string.account_chart_description,
-                        accountData[0].accountAttributes?.name, DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
-                        DateTimeUtil.getTodayDate())
-                getAccountTransaction(accountData[0].accountAttributes?.name ?: "")
-                setExpensesByCategory(currencyCode, accountData[0].accountAttributes?.name ?: "", currencySymbol)
+    private fun setLineChart(currencyCode: String, currencySymbol: String, accountName: String, accountBalance: Double){
+        zipLiveData(transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 1),
+                DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 1),
+                currencyCode, accountName),
+                transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 2),
+                        DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 2),
+                        currencyCode, accountName),
+                transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 3),
+                        DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 3),
+                        currencyCode, accountName),
+                transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 4),
+                        DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 4),
+                        currencyCode, accountName),
+                transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 5),
+                        DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 5),
+                        currencyCode, accountName),
+                transactionViewModel.getTransactionsByAccountAndCurrencyCodeAndDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
+                        DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
+                        currencyCode, accountName)).observe(this) { transactionData ->
+            val firstEntry = accountBalance.toBigDecimal().plus(transactionData.first)
+            val secondEntry = firstEntry.plus(transactionData.second)
+            val thirdEntry = secondEntry.plus(transactionData.third)
+            val fourthEntry = thirdEntry.plus(transactionData.fourth)
+            val fifthEntry = fourthEntry.plus(transactionData.fifth)
+            val sixthEntry = fifthEntry.plus(transactionData.sixth)
+            val lineChartEntries = arrayListOf(
+                    Entry(0f, firstEntry.toFloat()),
+                    Entry(1f, secondEntry.toFloat()),
+                    Entry(2f, thirdEntry.toFloat()),
+                    Entry(3f, fourthEntry.toFloat()),
+                    Entry(4f, fifthEntry.toFloat()),
+                    Entry(5f, sixthEntry.toFloat())
+            )
+            val dataSet = LineDataSet(lineChartEntries, accountName)
+            dataSet.apply {
+                setCircleColor(ContextCompat.getColor(requireContext(), R.color.primary))
+                valueTextColor = Color.BLUE
+                valueTextSize = 15f
+            }
+            val lineChartData = LineData(dataSet)
+            transactionLineChart.apply {
+                xAxis.granularity = 1f
+                xAxis.valueFormatter = IndexAxisValueFormatter(arrayListOf(
+                        DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 1)),
+                        DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 2)),
+                        DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 3)),
+                        DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 4)),
+                        DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 5)),
+                        DateTimeUtil.getDayAndMonth(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6))))
+                xAxis.setDrawLabels(true)
+                xAxis.setDrawAxisLine(false)
+                xAxis.setDrawGridLines(false)
+                data = lineChartData
+                axisRight.isEnabled = false
+                description.isEnabled = false
+                xAxis.position = XAxis.XAxisPosition.BOTTOM
+                data.isHighlightEnabled = false
+                setTouchEnabled(true)
+                setPinchZoom(true)
+                animateY(1000, Easing.EaseOutBack)
             }
         }
+        balanceHistoryCardText.text = resources.getString(R.string.account_chart_description,
+                accountName, DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
+                DateTimeUtil.getTodayDate())
     }
 
     private fun setExpensesByCategory(currencyCode: String, accountName: String, currencySymbol: String){
         zipLiveData(transactionViewModel.getTotalTransactionAmountByDateAndCurrency(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
-                DateTimeUtil.getTodayDate(), currencyCode, accountName, "Withdrawal"),transactionViewModel.getUniqueCategoryByDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
-                DateTimeUtil.getTodayDate(), currencyCode, accountName, "Withdrawal")).observe(this){ transactionData ->
-            if(transactionData.second.isNotEmpty()) {
-                setExpensesByBudget(currencyCode, accountName, currencySymbol, transactionData.first)
-                pieEntryArray = ArrayList(transactionData.second.size)
-                incomePieEntryArray = ArrayList(transactionData.second.size)
-                transactionData.second.forEachIndexed { _, uniqueMeow ->
-                    setIncomeByCategory(currencyCode, accountName, uniqueMeow, transactionData.first)
-                    transactionViewModel.getTransactionByDateAndCategoryAndCurrency(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
-                            DateTimeUtil.getTodayDate(), currencyCode, accountName,
-                            "Withdrawal", uniqueMeow).observe(this){ transactionAmount ->
-                        val percentageCategory: Double = transactionAmount.absoluteValue.roundToInt().toDouble().div(transactionData.first.absoluteValue.roundToInt().toDouble()).times(100)
-                        if (uniqueMeow == "null" || uniqueMeow == null) {
-                            pieEntryArray.add(PieEntry(percentageCategory.roundToInt().toFloat(), "No Category", transactionAmount))
-                        } else {
-                            pieEntryArray.add(PieEntry(percentageCategory.roundToInt().toFloat(), uniqueMeow, transactionAmount))
-                        }
-                        pieDataSet = PieDataSet(pieEntryArray, "")
-                        pieDataSet.valueFormatter = MpAndroidPercentFormatter()
-                        pieDataSet.colors = coloring
-                        pieDataSet.valueTextSize = 15f
-                        categoryPieChart.data = PieData(pieDataSet)
-                        categoryPieChart.invalidate()
+                DateTimeUtil.getTodayDate(), currencyCode, accountName, "Withdrawal"),
+                transactionViewModel.getUniqueCategoryByDate(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
+                        DateTimeUtil.getTodayDate(), currencyCode, accountName, "Withdrawal")).observe(this){ transactionData ->
+            setExpensesByBudget(currencyCode, accountName, currencySymbol, transactionData.first)
+            pieEntryArray = ArrayList(transactionData.second.size)
+            incomePieEntryArray = ArrayList(transactionData.second.size)
+            transactionData.second.forEachIndexed { _, uniqueMeow ->
+                setIncomeByCategory(currencyCode, accountName, uniqueMeow, transactionData.first)
+                transactionViewModel.getTransactionByDateAndCategoryAndCurrency(DateTimeUtil.getDaysBefore(DateTimeUtil.getTodayDate(), 6),
+                        DateTimeUtil.getTodayDate(), currencyCode, accountName,
+                        "Withdrawal", uniqueMeow).observe(this){ transactionAmount ->
+                    val percentageCategory: Double = transactionAmount.absoluteValue.roundToInt().toDouble().div(transactionData.first.absoluteValue.roundToInt().toDouble()).times(100)
+                    if (uniqueMeow == "null" || uniqueMeow == null) {
+                        pieEntryArray.add(PieEntry(percentageCategory.roundToInt().toFloat(), "No Category", transactionAmount))
+                    } else {
+                        pieEntryArray.add(PieEntry(percentageCategory.roundToInt().toFloat(), uniqueMeow, transactionAmount))
                     }
+                    pieDataSet = PieDataSet(pieEntryArray, "")
+                    pieDataSet.valueFormatter = MpAndroidPercentFormatter()
+                    pieDataSet.colors = coloring
+                    pieDataSet.valueTextSize = 15f
+                    categoryPieChart.data = PieData(pieDataSet)
+                    categoryPieChart.invalidate()
                 }
-                val decimalFormat = DecimalFormat(".##")
-                categoryPieChart.apply {
-                    description.text = currencySymbol + decimalFormat.format(transactionData.first)
-                    description.textSize = 15f
-                    legend.form = Legend.LegendForm.CIRCLE
-                    isDrawHoleEnabled = false
-                    setUsePercentValues(true)
-                    legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
-                    setTransparentCircleAlpha(0)
-                }
-                categoryPieChart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener{
-                    override fun onNothingSelected() {
-                    }
-
-                    override fun onValueSelected(e: Entry, h: Highlight) {
-                       toastInfo(currencySymbol + e.data)
-                    }
-
-                })
             }
+            categoryPieChart.apply {
+                description.text = currencySymbol + LocaleNumberParser.parseDecimal(transactionData.first, requireContext())
+                description.textSize = 15f
+                legend.form = Legend.LegendForm.CIRCLE
+                isDrawHoleEnabled = false
+                setUsePercentValues(true)
+                legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+                setTransparentCircleAlpha(0)
+            }
+            categoryPieChart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener{
+                override fun onNothingSelected() {
+                }
+                override fun onValueSelected(e: Entry, h: Highlight) {
+                    toastInfo(currencySymbol + e.data)
+                }
+            })
         }
     }
 
@@ -218,9 +213,8 @@ class AccountDetailFragment: BaseDetailFragment() {
                         budgetPieChart.invalidate()
                     }
                 }
-                val decimalFormat = DecimalFormat(".##")
                 budgetPieChart.apply {
-                    description.text = currencySymbol + decimalFormat.format(totalAmount)
+                    description.text = currencySymbol + LocaleNumberParser.parseDecimal(totalAmount, requireContext())
                     description.textSize = 15f
                     legend.form = Legend.LegendForm.CIRCLE
                     isDrawHoleEnabled = false
