fca2f821029c31aba607cefc2c56bc9852d679c8
Daniel Quah <danielquah148@yahoo.com>
2019-06-07 10:20:58 +0800

    Update root build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven("https://dl.bintray.com/florent37/maven")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("com.android.tools.build:gradle:3.4.1")
         classpath(kotlin("gradle-plugin", Dependencies.kotlinVersion))
         classpath("com.akaita.android:easylauncher:1.3.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
@@ -14,9 +14,20 @@ buildscript {
 
 allprojects {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com.android.*")
+                includeGroupByRegex ("androidx.*")
+                includeGroup("android.arch.work")
+                includeGroup("com.google.android.material")
+            }
+        }
         maven("https://jitpack.io")
         jcenter()
-        maven("https://dl.bintray.com/florent37/maven")
+        maven("https://dl.bintray.com/florent37/maven") {
+            content{
+                includeGroup("com.github.florent37")
+            }
+        }
     }
 }
