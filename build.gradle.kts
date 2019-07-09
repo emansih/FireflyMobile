7d162b7f83da2d18414a3c7d187fba53a4d91390
Daniel Quah <danielquah148@yahoo.com>
2019-07-09 19:45:32 +0800

    Update build script
    - Fix gradle script not changing resulting binary file name
    - Added org.owasp.dependencycheck

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,14 @@ buildscript {
         google()
         jcenter()
         maven("https://dl.bintray.com/florent37/maven")
+        mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.1")
         classpath(kotlin("gradle-plugin", Dependencies.kotlinVersion))
         classpath("com.akaita.android:easylauncher:1.3.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
+		classpath("org.owasp:dependency-check-gradle:5.1.0")
     }
 }
 
