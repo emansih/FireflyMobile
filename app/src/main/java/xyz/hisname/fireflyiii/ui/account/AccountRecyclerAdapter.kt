d25cef9e44c9556272fc4cd06fe4e099e26dbd57
Daniel Quah <danielquah148@yahoo.com>
2019-07-12 23:00:13 +0800

    Hide account number field if there is none

--- app/src/main/java/xyz/hisname/fireflyiii/ui/account/AccountRecyclerAdapter.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/account/AccountRecyclerAdapter.kt
@@ -3,6 +3,7 @@ package xyz.hisname.fireflyiii.ui.account
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.account_list_item.view.*
 import xyz.hisname.fireflyiii.R
@@ -38,7 +39,11 @@ class AccountRecyclerAdapter(private val items: MutableList<AccountData>, privat
             if(accountData?.currency_symbol != null){
                 currencySymbol = accountData.currency_symbol
             }
-            itemView.accountNumberText.text = accountData?.account_number
+            if(accountData?.account_number != null){
+                itemView.accountNumberText.text = accountData.account_number
+            } else {
+                itemView.accountNumberText.isVisible = false
+            }
             itemView.accountNameText.text = accountData?.name
             val amount = accountData?.current_balance?.toBigDecimal()?.toPlainString()
             if(amount != null){
