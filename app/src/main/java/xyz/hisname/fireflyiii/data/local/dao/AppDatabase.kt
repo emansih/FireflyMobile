92d6fb730a94bd85adad9b683c70c8791073d139
Daniel Quah <danielquah148@yahoo.com>
2019-09-27 22:58:14 +0800

    Increment database version

--- app/src/main/java/xyz/hisname/fireflyiii/data/local/dao/AppDatabase.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/data/local/dao/AppDatabase.kt
@@ -30,7 +30,7 @@ import xyz.hisname.fireflyiii.util.GsonConverterUtil
 @Database(entities = [PiggyData::class, PiggyFts::class, BillData::class, AccountData::class, CurrencyData::class,
     Transactions::class, TransactionIndex::class, CategoryData::class, CategoryFts::class, BudgetData::class,
     BudgetListData::class, BudgetListFts::class, TagsData::class, AttachmentData::class],
-        version = 11,exportSchema = false)
+        version = 12,exportSchema = false)
 @TypeConverters(GsonConverterUtil::class)
 abstract class AppDatabase: RoomDatabase() {
 
