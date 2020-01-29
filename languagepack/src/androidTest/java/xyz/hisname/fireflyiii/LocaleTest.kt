902f3e935182fc270f155cc4b6317bca00b1ea8c
Daniel Quah <danielquah148@yahoo.com>
2020-01-29 19:26:34 +0800

    Test for more supported languages

--- languagepack/src/androidTest/java/xyz/hisname/fireflyiii/LocaleTest.kt
+++ languagepack/src/androidTest/java/xyz/hisname/fireflyiii/LocaleTest.kt
@@ -1,12 +1,14 @@
 package xyz.hisname.fireflyiii
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
 import xyz.hisname.languagepack.LanguageChanger
 import xyz.hisname.languagepack.R
 
-
+@RunWith(AndroidJUnit4ClassRunner ::class)
 class LocaleTest {
 
     private val appContext by lazy { InstrumentationRegistry.getInstrumentation().context }
@@ -28,5 +30,47 @@ class LocaleTest {
     fun testSupportedLocale_English(){
         val contextWrapper = LanguageChanger.init(appContext, "en")
         assertEquals("Dashboard", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_SimplifiedChinese(){
+        val contextWrapper = LanguageChanger.init(appContext, "zh-rCN")
+        assertEquals("监控面板", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_TraditionalChinese(){
+        val contextWrapper = LanguageChanger.init(appContext, "zh-rTW")
+        assertEquals("監控面板", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_Dutch(){
+        val contextWrapper = LanguageChanger.init(appContext, "nl")
+        assertEquals("Dashboard", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_French(){
+        val contextWrapper = LanguageChanger.init(appContext, "fr")
+        assertEquals("Tableau de Bord", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_Italian(){
+        val contextWrapper = LanguageChanger.init(appContext, "it")
+        assertEquals("Cruscotto", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_Russian(){
+        val contextWrapper = LanguageChanger.init(appContext, "ru")
+        assertEquals("Сводка", contextWrapper.resources.getString(R.string.dashboard))
+    }
+
+    @Test
+    fun testSupportedLocale_Spanish(){
+        val contextWrapper = LanguageChanger.init(appContext, "es")
+        assertEquals("Panel de control", contextWrapper.resources.getString(R.string.dashboard))
     }
 }
