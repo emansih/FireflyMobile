311d3687127f346ecc8987155bd354f47bcedd62
emansih <danielquah148@yahoo.com>
2020-06-29 11:59:37 +1000

    Add test cases for date time parsing

--- app/src/test/java/xyz/hisname/fireflyiii/DateTimeTest.kt
+++ app/src/test/java/xyz/hisname/fireflyiii/DateTimeTest.kt
@@ -1,10 +1,20 @@
 package xyz.hisname.fireflyiii
 
-import junit.framework.TestCase.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import xyz.hisname.fireflyiii.util.DateTimeUtil
 
 class DateTimeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1593266400000", "1593267550000", "1593352799999", "1593320099999"])
+    fun testCalToString(epochTime: String){
+        val date = DateTimeUtil.getCalToString(epochTime)
+        assertEquals("2020-06-28", date)
+    }
 
     @Test
     fun testStartOfDay(){
@@ -16,5 +26,19 @@ class DateTimeTest {
     fun testEndOfDay(){
         val date = DateTimeUtil.getEndOfDayInCalendarToEpoch("2020-06-28")
         assertEquals("1593352799000", date)
+    }
+
+    @DisplayName("Convert yyyy-MM-dd to mm YY")
+    @Test
+    fun testMonthYear(){
+        val monthYear = DateTimeUtil.getMonthAndYear("2020-06-28")
+        assertEquals("Jun 2020", monthYear)
+    }
+
+    @DisplayName("Convert yyyy-MM-dd to dd/MM")
+    @Test
+    fun testDayMonth(){
+        val durationText = DateTimeUtil.getDayAndMonth("2020-06-28")
+        assertEquals("28/6", durationText)
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
         classpath("com.akaita.android:easylauncher:1.3.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
 		classpath("org.owasp:dependency-check-gradle:5.1.0")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
     }
 }
 
