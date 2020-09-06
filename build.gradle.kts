c708a56afd0835e35ba9a8fba2db3d8957dc420f
emansih <danielquah148@yahoo.com>
2020-09-06 18:02:05 +1000

    Switch easylauncher plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,13 @@ buildscript {
         google()
         jcenter()
         maven("https://dl.bintray.com/florent37/maven")
+        gradlePluginPortal()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
         classpath(kotlin("gradle-plugin", Dependencies.kotlinVersion))
-        classpath("com.akaita.android:easylauncher:1.3.1")
+        classpath("com.project.starter:easylauncher:3.2.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.28.0")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
     }
