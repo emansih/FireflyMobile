477713b3dd6c39cd890ac02a47365c44564045e8
emansih <danielquah148@yahoo.com>
2020-12-03 00:50:09 +1100

    Change bill details naming conventions

--- app/src/main/java/xyz/hisname/fireflyiii/ui/bills/details/BillDetailsFragment.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/bills/details/BillDetailsFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.commit
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
@@ -13,9 +15,10 @@ import kotlinx.android.synthetic.main.activity_base.*
 import kotlinx.android.synthetic.main.calendar_day.view.*
 import kotlinx.android.synthetic.main.fragment_bill_details.*
 import xyz.hisname.fireflyiii.R
-import xyz.hisname.fireflyiii.repository.models.bills.BillPaidDates
-import xyz.hisname.fireflyiii.repository.models.bills.BillPayDates
+import xyz.hisname.fireflyiii.repository.models.transaction.Transactions
 import xyz.hisname.fireflyiii.ui.base.BaseDetailFragment
+import xyz.hisname.fireflyiii.ui.transaction.TransactionAdapter
+import xyz.hisname.fireflyiii.ui.transaction.details.TransactionDetailsFragment
 import xyz.hisname.fireflyiii.util.DateTimeUtil
 import xyz.hisname.fireflyiii.util.extension.create
 import xyz.hisname.fireflyiii.util.extension.getCompatColor
@@ -29,9 +32,11 @@ import java.util.*
 class BillDetailsFragment: BaseDetailFragment() {
 
     private var selectedDate: LocalDate? = null
+    private val selectedDates = arrayListOf<LocalDate>()
     private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
     private val billDetailsViewModel by lazy { getImprovedViewModel(BillDetailsViewModel::class.java) }
     private val billId: Long by lazy { arguments?.getLong("billId") ?: 0  }
+    private val transactionAdapter by lazy { TransactionAdapter{ data -> itemClicked(data) } }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -41,35 +46,40 @@ class BillDetailsFragment: BaseDetailFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getPaidDates(DateTimeUtil.getStartOfMonth(), DateTimeUtil.getEndOfMonth())
+        setPaidCalendar()
+        setWidgets()
     }
 
     private fun getPaidDates(startDate: String, endDate: String){
         billDetailsViewModel.getPayList(billId, startDate, endDate).observe(viewLifecycleOwner){ billPayList ->
-            setPaidCalendar(billPayList)
+            billPayList.forEach {  billPayDates ->
+                selectedDates.add(billPayDates.payDates)
+            }
+            payDatesCalendarView.notifyCalendarChanged()
         }
     }
 
-    private fun setPaidCalendar(billPaidList: List<BillPayDates>){
+    private fun setPaidCalendar(){
         class DayViewContainer(view: View): ViewContainer(view) {
             lateinit var day: CalendarDay
             val onDayText = view.dayText
             init {
                 view.setOnClickListener {
                     selectedDate = day.date
-                    paidDatesCalendarView.notifyCalendarChanged()
+                    payDatesCalendarView.notifyCalendarChanged()
                 }
             }
         }
 
-        paidDatesCalendarView.dayBinder = object: DayBinder<DayViewContainer>{
+        payDatesCalendarView.dayBinder = object: DayBinder<DayViewContainer>{
             override fun bind(container: DayViewContainer, day: CalendarDay) {
                 container.day = day
                 val textView = container.onDayText
                 textView.text = day.date.dayOfMonth.toString()
                 if (day.owner == DayOwner.THIS_MONTH) {
-                    if (billPaidList.isNotEmpty()){
-                        billPaidList.forEach { data ->
-                            if(data.payDates == day.date){
+                    if (selectedDates.isNotEmpty()){
+                        selectedDates.forEach { data ->
+                            if(data == day.date){
                                 textView.setBackgroundColor(getCompatColor(R.color.md_red_400))
                             } else {
                                 textView.setTextColor(setDayNightTheme())
@@ -89,17 +99,31 @@ class BillDetailsFragment: BaseDetailFragment() {
 
             override fun create(view: View): DayViewContainer = DayViewContainer(view)
         }
-        paidDatesCalendarView.monthScrollListener = { month ->
+
+    }
+
+    private fun setWidgets(){
+        payDatesCalendarView.monthScrollListener = { month ->
             val title = "${monthTitleFormatter.format(month.yearMonth)} ${month.yearMonth.year}"
             paidDatesHeaderText.text = title
+            getPaidDates(month.yearMonth.atDay(1).toString(), month.yearMonth.atEndOfMonth().toString())
         }
         val currentMonth = YearMonth.now()
         val startMonth = currentMonth.minusYears(40)
         val endMonth = currentMonth.plusYears(30)
-        paidDatesCalendarView.setup(startMonth, endMonth,
+        payDatesCalendarView.setup(startMonth, endMonth,
                 WeekFields.of(Locale.getDefault()).firstDayOfWeek)
-        paidDatesCalendarView.scrollToMonth(currentMonth)
-        paidDatesCalendarView.updateMonthConfiguration()
+        payDatesCalendarView.scrollToMonth(currentMonth)
+        payDatesCalendarView.updateMonthConfiguration()
+    }
+
+    private fun itemClicked(data: Transactions){
+        parentFragmentManager.commit {
+            add(R.id.fragment_container, TransactionDetailsFragment().apply {
+                arguments = bundleOf("transactionJournalId" to data.transaction_journal_id)
+            })
+            addToBackStack(null)
+        }
     }
 
     private fun setDayNightTheme(): Int{
