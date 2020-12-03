6366c50c3ae889f96e9217f10a9c7a42d973df41
emansih <danielquah148@yahoo.com>
2020-12-03 16:36:33 +1100

    Remove unused models

--- app/src/main/java/xyz/hisname/fireflyiii/repository/models/BaseDetailModel.kt
+++
@@ -1,5 +0,0 @@
-package xyz.hisname.fireflyiii.repository.models
-
-import android.graphics.drawable.Drawable
-
-data class BaseDetailModel(var title: String?,var  subTitle: String?, var image: Drawable?)
