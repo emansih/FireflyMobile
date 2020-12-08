063b73258dfb143cf1972ec561e9036d57a15bf3
emansih <danielquah148@yahoo.com>
2020-12-08 19:33:55 +1100

    Added and remove some code to show markdown fragment when coming from add transaction activity

--- app/src/main/java/xyz/hisname/fireflyiii/ui/transaction/addtransaction/AddTransactionActivity.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/transaction/addtransaction/AddTransactionActivity.kt
@@ -3,14 +3,11 @@ package xyz.hisname.fireflyiii.ui.transaction.addtransaction
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import androidx.fragment.app.commit
 import kotlinx.android.synthetic.main.activity_add_transaction.*
 import xyz.hisname.fireflyiii.R
 import xyz.hisname.fireflyiii.ui.base.BaseActivity
 import xyz.hisname.fireflyiii.util.extension.hideKeyboard
-import xyz.hisname.fireflyiii.util.extension.showCase
 
 class AddTransactionActivity: BaseActivity() {
 
@@ -21,30 +18,12 @@ class AddTransactionActivity: BaseActivity() {
         setContentView(R.layout.activity_add_transaction)
         hideKeyboard()
         setBottomNav()
-        setHelpText()
-        backstackCounter()
         when (transactionType) {
             "Withdrawal" -> transactionBottomView.selectedItemId = R.id.action_withdraw
             "Deposit" -> transactionBottomView.selectedItemId = R.id.action_deposit
             "Transfer" -> transactionBottomView.selectedItemId = R.id.action_transfer
             else -> transactionBottomView.selectedItemId = R.id.action_withdraw
         }
-    }
-
-    private fun backstackCounter(){
-        supportFragmentManager.addOnBackStackChangedListener {
-            if(supportFragmentManager.backStackEntryCount == 1){
-                addTransaction_coordinatorLayout.isInvisible = true
-            } else if(supportFragmentManager.backStackEntryCount == 0){
-                addTransaction_coordinatorLayout.isVisible = true
-            }
-        }
-
-    }
-
-    private fun setHelpText(){
-        showCase(R.string.transactions_create_switch_box, "bottomNavigationShowCase",
-                transactionBottomView, false).show()
     }
 
     private fun setBottomNav(){
