0259e737d250928e7ba67aa32689ff4939231510
Arnau Mora <arnyminer.z@gmail.com>
2022-11-24 19:32:51 +0100

    Null-safety for `Animator` in the listener
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/xyz/hisname/fireflyiii/ui/base/BaseAddObjectFragment.kt
+++ app/src/main/java/xyz/hisname/fireflyiii/ui/base/BaseAddObjectFragment.kt
@@ -27,7 +27,7 @@ import androidx.core.view.isVisible
 import xyz.hisname.fireflyiii.util.animation.BakedBezierInterpolator
 import kotlin.math.max
 
-abstract class BaseAddObjectFragment: BaseFragment() {
+abstract class BaseAddObjectFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,21 +35,21 @@ abstract class BaseAddObjectFragment: BaseFragment() {
         setWidgets()
     }
 
-    protected fun unReveal(rootView: View){
+    protected fun unReveal(rootView: View) {
         val x = rootView.width / 2
         val y = rootView.height / 2
         val finalRadius = (max(rootView.width, rootView.height) * 1.1).toFloat()
-        val circularReveal= ViewAnimationUtils.createCircularReveal(
-                rootView, x, y,finalRadius, 0f)
+        val circularReveal = ViewAnimationUtils.createCircularReveal(
+            rootView, x, y, finalRadius, 0f
+        )
         circularReveal.duration = 400
         circularReveal.interpolator = BakedBezierInterpolator.FADE_OUT_CURVE
-        circularReveal.addListener(object : AnimatorListenerAdapter(){
-            override fun onAnimationEnd(animation: Animator?) {
+        circularReveal.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
                 super.onAnimationEnd(animation)
                 try {
                     parentFragmentManager.popBackStack()
-                } catch(illegal: IllegalStateException){
-
+                } catch (_: IllegalStateException) {
                 }
                 rootView.isVisible = false
                 fragmentContainer.isVisible = true
